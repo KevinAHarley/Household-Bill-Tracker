3c2623ac7a043b9f00275ef0e6b9aad5377d0a45
Kevin Harley <kevin.harley99@gmain.com>
2023-10-24 13:13:39 +0100

    Create SwitchButton component

---
+++ src/components/SwitchButton/SwitchButton.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Pressable, Text, View, ViewStyle } from "react-native";
+
+import styles from "./SwitchButton.styles";
+
+type SwitchButtonProps = {
+  leftButtonText: string;
+  rightButtonText: string;
+  containerStyle?: ViewStyle;
+  onChange?: () => void;
+};
+
+const SwitchButton = ({
+  leftButtonText,
+  rightButtonText,
+  containerStyle,
+  onChange,
+}: SwitchButtonProps) => {
+  const [selected, setSelected] = useState(0);
+
+  const leftSelected = selected === 0;
+  const rightSelected = selected === 1;
+
+  useEffect(() => {
+    onChange && onChange();
+  }, [selected]);
+
+  return (
+    <View style={[styles.contentContainer, containerStyle]}>
+      <Pressable
+        style={[leftSelected ? [styles.selectedContainer] : styles.container]}
+        onPress={() => setSelected(0)}
+      >
+        <Text style={leftSelected ? styles.selectedText : styles.text}>
+          {leftButtonText}
+        </Text>
+      </Pressable>
+      <Pressable
+        style={[rightSelected ? [styles.selectedContainer] : styles.container]}
+        onPress={() => setSelected(1)}
+      >
+        <Text style={rightSelected ? styles.selectedText : styles.text}>
+          {rightButtonText}
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default SwitchButton;
