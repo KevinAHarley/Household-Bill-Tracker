220dae79851b5e788869b407fc07c0fee575a387
Kevin Harley <kevin.harley99@gmain.com>
2023-10-25 15:17:48 +0100

    Create ProgressBar component

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,61 @@
+import { Text, View } from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+import config from "config";
+
+import styles from "./ProgressBar.styles";
+
+type ProgressBarProps = {
+  goal: number;
+  progress: number;
+};
+
+const ProgressBar = ({ goal, progress }: ProgressBarProps) => {
+  const currentProgress = progress / goal;
+  const currentProgressAsPercentage = currentProgress * 100 + "%";
+  const oppositeProgress = 1 - currentProgress;
+  const oppositeProgressAsPercentage = oppositeProgress * 40 - 5 + "%";
+
+  const goalTextCutoff = currentProgress * 100 < 70;
+  const progressTextCutoff = currentProgress * 100 > 30;
+
+  const goalBarLengthCheck = (goal - progress).toString().length > 4;
+  const progressBarLengthCheck = progress.toString().length > 4;
+
+  return (
+    <View>
+      <View style={styles.goalContainer}>
+        <View style={styles.goalTextContainer}>
+          {goalTextCutoff && !goalBarLengthCheck ? (
+            // Margin right doesn't accept the % value in current format, needs reworked
+            <Text
+              style={[
+                { marginRight: oppositeProgressAsPercentage },
+                styles.goalText,
+              ]}
+            >
+              £{goal - progress}
+            </Text>
+          ) : null}
+        </View>
+      </View>
+      <LinearGradient
+        colors={[config.colors.teal, config.colors.lightBlue]}
+        start={{ x: 0, y: 0 }}
+        style={[
+          currentProgress * 100 > 10 && {
+            width: currentProgressAsPercentage,
+          },
+          styles.progressTextContainer,
+        ]}
+      >
+        {progressTextCutoff && !progressBarLengthCheck ? (
+          <Text style={styles.progressText}>£{progress}</Text>
+        ) : null}
+      </LinearGradient>
+    </View>
+  );
+};
+
+export default ProgressBar;
