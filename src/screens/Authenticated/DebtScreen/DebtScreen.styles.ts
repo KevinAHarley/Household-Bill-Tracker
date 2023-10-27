cd9936c0b9600d303f9681c1dc15589553850c6b
Kevin Harley <kevin.harley99@gmain.com>
2023-10-27 12:34:21 +0100

    Add components to DebtScreen

--- src/screens/Authenticated/DebtScreen/DebtScreen.styles.ts
+++ src/screens/Authenticated/DebtScreen/DebtScreen.styles.ts
@@ -1,5 +1,44 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+import config from "config";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    paddingBottom: 16,
+  },
+  headerInnerContainer: {
+    flex: 1,
+  },
+  headerText: {
+    color: config.colors.secondary,
+    fontSize: 24,
+  },
+  editButton: {
+    width: 88,
+    height: 104,
+    marginLeft: 10,
+  },
+  cardContainer: {
+    flexDirection: "row",
+  },
+  card: {
+    marginBottom: 16,
+    flex: 1,
+  },
+  scrollView: {
+    paddingVertical: 32,
+  },
+  providerText: {
+    fontSize: 18,
+    color: config.colors.secondary,
+    marginBottom: 32,
+  },
+});
 
 export default styles;
--- src/screens/Authenticated/DebtScreen/DebtScreen.tsx
+++ src/screens/Authenticated/DebtScreen/DebtScreen.tsx
@@ -1,11 +1,53 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
+import Avatar from "components/Avatar";
 import Background from "components/Background";
+import Button from "components/Button";
+import Card from "components/Card";
+import ProgressBar from "components/ProgressBar";
+import mockedDebt from "mocks/mockedDebt";
+
+import styles from "./DebtScreen.styles";
 
 const DebtScreen = () => {
+  const [selected, setSelected] = useState<string>("");
+
   return (
-    <Background>
-      <Text>Debt Screen</Text>
+    <Background style={styles.container}>
+      <View style={styles.headerContainer}>
+        <View style={styles.headerInnerContainer}>
+          <Text style={styles.headerText}>Debt</Text>
+        </View>
+        <Avatar initials="KH" size="S" />
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollView}
+      >
+        {mockedDebt.map(({ amount, repayment, provider, id }) => (
+          <View id={id} style={styles.cardContainer}>
+            <Card
+              style={styles.card}
+              onPress={() =>
+                selected === id ? setSelected("") : setSelected(id)
+              }
+            >
+              <Text style={styles.providerText}>{provider}</Text>
+              <ProgressBar goal={amount} progress={repayment} />
+            </Card>
+            {selected === id && (
+              <Button
+                title="Edit"
+                type="secondary"
+                onPress={() => null}
+                style={styles.editButton}
+              />
+            )}
+          </View>
+        ))}
+      </ScrollView>
+      <Button title="Add" type="primary" onPress={() => null} />
     </Background>
   );
 };
